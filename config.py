688aeb0bf60500fdfce1275ddcf2fb1d647e0b28
ak4422920 <ak4422920@gmail.com>
2025-06-03 17:24:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,8 +40,8 @@ START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
 
 #--------------------------------------------
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "cuty.io")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "f351cc7a7bcfdfca8ebd5e084e30be2b1daa6d24")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 21600)) # Add time in seconds
 TUT_VID = os.environ.get("TUT_VID","https://t.me/how_to_open_link_ak/14")
 
